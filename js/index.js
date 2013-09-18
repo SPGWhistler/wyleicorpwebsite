163741980cb73645a7283784f10e24148aa6b60e
Tony Petty <tpetty@appmobi.com>
2013-09-18 11:54:43 -0400

    Working.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 /*global $ */
 (function () {
 	$(function () {
-		var redraw, visible;
-		redraw = false;
-		visible = false;
+		var redraw, video_visible;
+		redraw = true;
+		video_visible = false;
 		$('#video_image').click(function () {
 			var $video;
 			$video = $('#video_container');
@@ -11,25 +11,32 @@
 				background: '#000'
 			});
 			$video.show();
-			visible = true;
+			video_visible = true;
 			redraw = true;
 		});
 		$(window).resize(function () {
 			redraw = true;
 		});
 		window.setInterval(function () {
-			if (redraw && visible) {
-				var $video, $image, offset, left, top;
-				$video = $('#video_container');
-				$image = $('#video_image');
-				offset = $image.offset();
-				left = $image.width() * 0.09;
-				top = $image.height() * 0.1;
-				$video.css({
-					top: offset.top + top,
-					left: offset.left + left,
-					width: $image.width() - (left * 2),
-					height: $image.height() - (top * 2)
+			var $video, $image, offset, left, top, $vc;
+			if (redraw) {
+				if (video_visible) {
+					$video = $('#video_container');
+					$image = $('#video_image');
+					offset = $image.offset();
+					left = $image.width() * 0.09;
+					top = $image.height() * 0.1;
+					$video.css({
+						top: offset.top + top,
+						left: offset.left + left,
+						width: $image.width() - (left * 2),
+						height: $image.height() - (top * 2)
+					});
+				}
+				$vc = $('.vertical_centered');
+				$vc.each(function (index, elmt) {
+					var height = $(elmt).parent().parent().find('div.col-xs-9').height();
+					$(elmt).height(height);
 				});
 			}
 			redraw = false;
