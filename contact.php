5d4c1395ca683ea3905c74a5b9a82d565e257407
Tony Petty <tpetty@appmobi.com>
2013-09-19 13:55:26 -0400

    Contact form.

--- contact.php
+++ contact.php
@@ -1,4 +1,23 @@
 <?php
-mail();
+include "classes/phpmailer/class.phpmailer.php";
+$mail = new PHPMailer;
+$mail->IsSMTP();
+$mail->Host = 'email-smtp.us-east-1.amazonaws.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'AKIAJSWUXNDIETFEUVGQ';
+$mail->Password = 'AnES1DLuK8VaFIgB9erfpekyhSn3RzbA9Z65JStezN93';
+$mail->SMTPSecure = 'tls';
+$mail->From = $_GET['email'];
+$mail->FromName = $_GET['name'];
+$mail->AddReplyTo($_GET['email']);
+$mail->AddAddress('tony@wylei.com');
+$mail->WordWrap = 50;
+$mail->IsHTML(false);
+$mail->Subject = 'Wylei Contact Form';
+$body = 'phone: ' . $_GET['phone'];
+$body += ' name: ' . $_GET['name'];
+$body += ' email: ' . $_GET['email'];
+$mail->Body = $body;
+$mail->Send();
 header('Location: index.html');
 ?>
